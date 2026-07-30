081773f13fa8e11ec75e7f1b93da7d781b5bdd7e
Angel Christian <angelchristianltorrejos@gmail.com>
2026-07-30 16:41:37 +0800

    initial commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'expo-router'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>Sign In</Text>
+            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Sign Up</Link>
+        </View>
+    )
+}
+
+export default SignIn
